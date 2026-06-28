36bc1fefbefd55558f066b18c14c35e9df914581
cskying <ikun0702@gmail.com>
2026-06-28 08:26:57 +0800

    Add files via upload

---
+++ deepseek_javascript_20260628_a8db39.js
@@ -0,0 +1,10 @@
+// Navigate to homepage
+function goHome() {
+    window.location.href = 'https://cskying.github.io/chess1academy/';
+}
+
+// Usage
+goHome();
+
+// Or with a button
+<button onclick="goHome()">Home</button>
